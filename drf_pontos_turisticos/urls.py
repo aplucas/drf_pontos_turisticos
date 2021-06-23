8dd2f0c876687b3948c69d96e6db379f16a59719
l.alexandre100@gmail.com <aplucas>
2021-06-23 20:44:22 -0300

    endpoint de atracao

--- drf_pontos_turisticos/urls.py
+++ drf_pontos_turisticos/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracao', AtracaoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
