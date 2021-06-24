8d66f59e3d944978e73d7d60a4523cc544894a29
l.alexandre100@gmail.com <aplucas>
2021-06-24 10:18:14 -0300

    Objetos aninhados no endpoint de pontos turisticos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
+from atracoes.api.serializers import AtracaoSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from comentarios.api.serializers import ComentarioSerializer
 from core.models import PontoTuristico
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvaliacaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto']
+        fields = ['id', 'nome', 'descricao', 'atracoes', 'foto',
+                  'comentarios', 'avaliacoes', 'endereco', 'foto']
